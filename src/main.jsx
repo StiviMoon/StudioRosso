a7c10f7a4d7498cad757f224a7b55c90be2de18b
StiviMoon <johanprogamer01@gmail.com>
2025-08-27 18:34:26 -0500

    Fix: Update import path for Vercel Analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
