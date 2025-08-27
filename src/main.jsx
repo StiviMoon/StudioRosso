cb4e89fa44977a7e391d3cc68200aa0e6c754b29
StiviMoon <johanprogamer01@gmail.com>
2025-08-27 18:33:17 -0500

    Refactor: Update Vercel configuration for redirects and headers
    - Changed "routes" to "redirects" in vercel.json for better clarity and consistency.
    - Updated source and destination keys for various static assets.
    - Added headers for sitemap.xml, robots.txt, and other assets to improve caching and content delivery.

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import { HelmetProvider } from 'react-helmet-async';
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App/>
+        <App />
         <SpeedInsights />
+        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
